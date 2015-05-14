6f7c3ed0fb4f1a65a4b446607c788e4b02e59f7c
CIT Student <citstudent@Bueno.local>
2015-05-14 10:49:02 +1000

    moved the change theme button, fixed some css styling

--- tictactoe.js
+++ tictactoe.js
@@ -40,87 +40,85 @@ $(document).ready(function() {
 
   //Checks the winning values for all the squares(Long and chunky will try and do with arrays if I can work it out)
   var evaluateWinner = function() {
-    //squareOne Winning options...
     if (($squareOne.text() === "X") && ($squareTwo.text() === "X") && ($squareThree.text() === "X")) {
       $('#one, #two, #three').addClass('winningSquares').fadeIn(5000)
       return true;
-    }
+    };
     if (($squareOne.text() === "O") && ($squareTwo.text() === "O") && ($squareThree.text() === "O")) {
       $('#one, #two, #three').addClass('winningSquares').fadeIn(5000)
       return true;
-    }
+    };
     if (($squareOne.text() === "X") && ($squareFour.text() === "X") && ($squareSeven.text() === "X")) {
       $('#one, #four, #seven').addClass('winningSquares').fadeIn(5000)
       return true;
-    }
+    };
     if (($squareOne.text() === "O") && ($squareFour.text() === "O") && ($squareSeven.text() === "O")) {
       $('#one, #four, #seven').addClass('winningSquares').fadeIn(5000)
       return true;
-    }
+    };
     if (($squareOne.text() === "X") && ($squareFive.text() === "X") && ($squareNine.text() === "X")) {
       $('#one, #five, #nine').addClass('winningSquares').fadeIn(5000)
       return true;
-    }
+    };
     if (($squareOne.text() === "O") && ($squareFive.text() === "O") && ($squareNine.text() === "O")) {
       $('#one, #five, #nine').addClass('winningSquares').fadeIn(5000)
       return true;
-    }
+    };
 
     //squareTwo Winning options...
 
     if (($squareFive.text() === "X") && ($squareTwo.text() === "X") && ($squareEight.text() === "X")) {
       $('#five, #two, #eight').addClass('winningSquares').fadeIn(5000)
       return true;
-    }
+    };
     if (($squareFive.text() === "O") && ($squareTwo.text() === "O") && ($squareEight.text() === "O")) {
       $('#five, #two, #eight').addClass('winningSquares').fadeIn(5000)
       return true;
-    }
+    };
     if (($squareFive.text() === "X") && ($squareFour.text() === "X") && ($squareSix.text() === "X")) {
       $('#five, #four, #six').addClass('winningSquares').fadeIn(5000)
       return true;
-    }
+    };
     if (($squareFive.text() === "O") && ($squareFour.text() === "O") && ($squareSix.text() === "O")) {
       $('#five, #four, #six').addClass('winningSquares').fadeIn(5000)
       return true;
-    }
+    };
     if (($squareFive.text() === "X") && ($squareThree.text() === "X") && ($squareSeven.text() === "X")) {
       $('#five, #three, #seven').addClass('winningSquares').fadeIn(5000)
       return true;
-    }
+    };
     if (($squareFive.text() === "O") && ($squareThree.text() === "O") && ($squareSeven.text() === "O")) {
       $('#five, #three, #seven').addClass('winningSquares').fadeIn(5000)
       return true;
-    }
+    };
 
     //squareThree Winning options...
 
     if (($squareNine.text() === "X") && ($squareThree.text() === "X") && ($squareSix.text() === "X")) {
       $('#nine, #three, #six').addClass('winningSquares').fadeIn(5000)
       return true;
-    }
+    };
     if (($squareNine.text() === "O") && ($squareThree.text() === "O") && ($squareSix.text() === "O")) {
       $('#nine, #three, #six').addClass('winningSquares').fadeIn(5000)
       return true;
-    }
+    };
     if (($squareNine.text() === "X") && ($squareFive.text() === "X") && ($squareOne.text() === "X")) {
       $('#nine, #five, #one').addClass('winningSquares').fadeIn(5000)
       return true;
-    }
+    };
     if (($squareNine.text() === "O") && ($squareFive.text() === "O") && ($squareOne.text() === "O")) {
       $('#nine, #five, #one').addClass('winningSquares').fadeIn(5000)
       return true;
-    }
+    };
     if (($squareNine.text() === "X") && ($squareSeven.text() === "X") && ($squareEight.text() === "X")) {
       $('#nine, #seven, #eight').addClass('winningSquares').fadeIn(5000)
       return true;
-    }
+    };
     if (($squareNine.text() === "O") && ($squareSeven.text() === "O") && ($squareEight.text() === "O")) {
       $('#nine, #seven, #eight').addClass('winningSquares').fadeIn(5000)
       return true;
-    }
-  }
-
+    };
+  };
 
   var getWinner = function() {
     if (evaluateWinner()) {
@@ -168,21 +166,36 @@ $(document).ready(function() {
   $('.square').one('click', playerMove)
 
   //Click event to bring up the instructions page
-  $('#instruct h2').on('click', function(){
-    console.log('instruct click is firing')
-    $('#instructHidden').show('puff')
-    $('#boardAndInstructions').hide('puff')
+  $('#instruct #instructClick').on('click', function(){
+    console.log('instruct click is firing');
+    $('#instructHidden').show('puff');
+    $('#boardAndInstructions').hide('puff');
   });
+
+  $('#instruct #themeClick').on('click', function(){
+    console.log('theme click is firing');
+    $('#boardAndInstructions').hide('puff');
+    $('#changeThemeHidden').show('puff');
+  });
+
+  //Closes whatever the parent element is of the close button
+  $('.closeButton').on('click', function(){
+    $('#changeThemeHidden').hide('puff');
+    $('#boardAndInstructions').show('puff');
+  });
+
+
 
   //Click event to hide the instructions and bring up play page
   $('#instructHidden h2').on('click', function(){
-    console.log('instructions click is firing')
-    $('#instructHidden').hide('puff')
-    $('#boardAndInstructions').show('puff')
+    console.log('instructions click is firing');
+    $('#instructHidden').hide('puff');
+    $('#boardAndInstructions').show('puff');
   });
 
+
   //This will reset all the squares and have the game run again.
-  $('#reset').on('click', function(){
+  $('#playAgain').on('click', function(){
     console.log('Reset is firing')
     $('#winnerHidden').hide('puff');
     clearBoard()
@@ -202,12 +215,6 @@ $(document).ready(function() {
     $('#gamesOWon').text('Naughts have won '+ oWins)
   });
 
-  //Brings up the changeThemeHidden div to select theme change
-  $('#theme').on('click', function(){
-    console.log('style change is firing')
-    $('#winnerHidden').hide()
-    $('#changeThemeHidden').show('puff');
-  });
   //Changes theme to field
   $('#changeThemeHidden #field').on('click', function(){
     $('#themeSelect').attr('href', 'styles.css')
@@ -221,18 +228,8 @@ $(document).ready(function() {
     console.log('ocean click is firing')
     $('#themeSelect').attr('href', 'ocean.css')
   });
-   //Changes theme to space
+   //Changes theme to WhatTheFuck
   $('#changeThemeHidden #WhatTheFuck').on('click', function(){
     $('#themeSelect').attr('href', '/Styles/WhatTheFuck.css')
   });
-  //Closes whatever the parent element is of the close button
-  $('.closeButton').on('click', function(){
-    $(this).parent().hide('puff')
-    $('#winnerHidden').show('puff')
-  });
-
-
-
-
-
 });
